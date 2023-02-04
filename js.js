6e0562dcffcd0289d5a2a5b09f5aad005e2a6f8f
adamfoks <100434460+adamfoks@users.noreply.github.com>
2023-02-04 21:05:45 +0100

    more function example

--- js.js
+++ js.js
@@ -54,7 +54,7 @@ printSquare(4); */
 
 // wypisuje trójkąt równoramienny z gwiazdek o określonym rozmiarze np 3:
 
-function printStars(num) {
+/* function printStars(num) {
     let stars = "";
     for (let i = 0; i < num; i++) {
         stars += "*";
@@ -68,4 +68,74 @@ function printTriangle(size) {
         printStars(i);
     }
 }
-printTriangle(3)
+printTriangle(3) */
+
+
+
+//funkcja do kwadratu
+
+/* function square(x) {
+    return x * x;
+}
+console.log(square(4)); */
+
+
+
+//funkcja do kwadratu, również liczny ujemne
+/* function absolut(x) {
+    if (x >= 0) {
+        return x;
+    } else {
+        return -x;
+    }
+}
+console.log (absolut(5)); */
+
+
+
+
+//funkcja silni z np 4
+/* function factorial(num) {
+    if (num <= 1) {
+        return 1;
+    }
+    let acc = 1;
+    for (var i = 2; i <= num; i++) {
+        acc *= i;
+    }
+    return acc;
+}
+console.log(factorial()); */
+
+
+
+
+// funkcja zamienia liczbę dni na liczbę milisekund (jedna sekunda to 1000 milisekund).
+/* function daysToMillis(days) {
+    return days * 24 * 60 * 60 * 1000;
+}
+console.log(daysToMillis(1)); */
+
+
+// oblicza wielkość pola powierzchni trójkąta prostokątnego,na podstawie długości jego prostopadłych boków (wzór to a * b / 2).
+/* function triangleArea(a, b) {
+    return a * b / 2;
+}
+console.log(triangleArea(10, 20)); */
+
+
+
+//zwracana większą z trzech wartości.
+function biggestOf(a, b, c) {
+    if (a >= b && a >= c) {
+        return a;
+    } else if (b >= c) {
+        return b;
+    } else {
+        return c;
+    }
+}
+console.log(biggestOf(5, 2, 10));
+
+
+
