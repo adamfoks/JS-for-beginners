65e22d822912dd87bf7b5e50b5ecce0ddd227712
adamfoks <100434460+adamfoks@users.noreply.github.com>
2023-02-05 21:55:05 +0100

    new object tasks

--- js.js
+++ js.js
@@ -126,7 +126,7 @@ console.log(triangleArea(10, 20)); */
 
 
 //zwracana większą z trzech wartości.
-function biggestOf(a, b, c) {
+/* function biggestOf(a, b, c) {
     if (a >= b && a >= c) {
         return a;
     } else if (b >= c) {
@@ -135,7 +135,93 @@ function biggestOf(a, b, c) {
         return c;
     }
 }
-console.log(biggestOf(5, 2, 10));
+console.log(biggestOf(5, 2, 10)); */
 
+
+
+// losowanie dowolenj liczby od 0 do 1 bez 1
+// console.log(Math.random());
+
+
+//42 ∗ π
+//console.log(Math.pow(4, 2) * Math.PI);
+
+
+
+//log10(20∗30+40)
+//console.log(Math.log10(20 * 30 + 40));
+
+
+
+//log210−5
+//console.log(Math.log2(Math.pow(10, -5)));
+
+
+
+
+// |(−3)7|(te poziome kreski to w matematyce wartość bezwzględna)
+// console.log(Math.abs(Math.pow(-3, 7)));
+
+
+//funkcje jako warotoćsi
+/* function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 2));
+console.log(add);
+console.log(typeof add);
+
+const plus = add;
+console.log(plus(1, 2)); */
+
+
+
+
+//OBIEKTY
+
+/* const user = {
+    name: "Kuba",
+    surname: "Wędrowicz",
+    address: {
+        country: "Polska",
+        city: "Stary Majdan",
+        postal: "22-120",
+        street: "null"
+    }
+};
+
+const book = {
+    title: "Karpie bijem",
+    releaseYear: "2019"
+};
+
+console.log(user.surname);
+
+user.name = "Jakub";
+
+console.log(user.name);
+console.log(user.address.city);
+
+user.address.street = "Baganowska";
+console.log(user.address.street);
+console.log(book.releaseYear); */
+
+
+
+
+
+let obj = {name: "Alek"};
+const obj2 = obj;
+
+console.log(obj.name);
+console.log(obj2.name);
+
+const obj3 = {name: obj.name};
+console.log(obj3);
+
+obj = {size: "S"};
+console.log(obj.name);
+console.log(obj.size);
+console.log(obj2.name);
 
 
