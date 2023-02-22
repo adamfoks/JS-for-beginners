467b2dd77f2b48773c892d73da95cc519d61cb60
adamfoks <100434460+adamfoks@users.noreply.github.com>
2023-02-22 21:21:11 +0100

    noFixed tasks

--- kurs-java-script.js
+++ kurs-java-script.js
@@ -1,16 +1,31 @@
 // Trainning: http://kursjs.pl/
 
-let str = "";
+const nr = 123.456789;
 
-for (let i=0; i<4; i++) {
-    for (let j=0; j<6; j++) {
-        if (i===0 || i===3 || j===0 || j===5) {
-            str += "*";
-        } else {
-            str += "-";
-        }
-    }
-    str += "\n";
-}
+nr.toFixed()      // "123"
+nr.toFixed(0)     // "123"
+nr.toFixed(1)     // "123.5"
+nr.toFixed(2)     // "123.46"
+nr.toFixed(3)     // "123.457"
+nr.toFixed(4)     // "123.4568"
+nr.toFixed(5)     // "123.45679"
+nr.toFixed(6)     // "123.456789"
+nr.toFixed(7)     // "123.4567890"
+nr.toFixed(8)     // "123.45678900"
+nr.toFixed(9)     // "123.456789000"
+nr.toFixed(10)    // "123.4567890000"
+nr.toFixed(11)    // "123.45678900000"
 
-console.log(str);
+nr.toPrecision()      // "123.456789"
+// nr.toPrecision(0)     // błąd, parametr musi być z przedziału 1-100
+nr.toPrecision(1)     // "1e+2"
+nr.toPrecision(2)     // "1.2e+2"
+nr.toPrecision(3)     // "123"
+nr.toPrecision(4)     // "123.5"
+nr.toPrecision(5)     // "123.46"
+nr.toPrecision(6)     // "123.457"
+nr.toPrecision(7)     // "123.4568"
+nr.toPrecision(8)     // "123.45679"
+nr.toPrecision(9)     // "123.456789"
+nr.toPrecision(10)    // "123.4567890"
+nr.toPrecision(11)    // "123.45678900"
