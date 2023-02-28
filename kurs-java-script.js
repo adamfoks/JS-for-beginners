fa199f50e041506ba38620d4c3b2979c6013bd75
adamfoks <100434460+adamfoks@users.noreply.github.com>
2023-02-28 22:26:37 +0100

    tasks with function

--- kurs-java-script.js
+++ kurs-java-script.js
@@ -1,33 +1,16 @@
 // Trainning: http://kursjs.pl/
 
-const level = [
-    [1, 1, 0, 0, 2, 2, 0, 0, 1, 1],
-    [1, 0, 0, 0, 2, 2, 0, 0, 0, 1],
-    [1, 0, 1, 1, 2, 2, 1, 1, 0, 1],
-    [1, 0, 2, 2, 2, 2, 2, 2, 0, 1],
-    [2, 2, 2, 2, 1, 1, 2, 2, 2, 2],
-    [2, 2, 2, 2, 1, 1, 2, 2, 2, 2],
-    [1, 0, 2, 2, 2, 2, 2, 2, 0, 1],
-    [1, 0, 1, 1, 2, 2, 1, 1, 0, 1],
-    [1, 0, 0, 0, 2, 2, 0, 0, 0, 1],
-    [1, 1, 0, 0, 2, 2, 0, 0, 1, 1]
-];
-
-
-let str = "";
-
-for (const subTab of level) {
-    //pod subTab mamy każdą kolejną podtablicę
-
-    for (const el of subTab) {
-        switch (el) {
-            case 0 : str += "🟩"; break;
-            case 1 : str += "🟫"; break;
-            case 2 : str += "⬛"; break;
-        }
+function getStatus(number) {
+    if (number < 20) {
+        return "bad"
     }
 
-    str += "\n";
+    if (number < 30) {
+        return "medium"
+    }
+
+    return "good"
 }
 
-console.log(str);
+console.log(getStatus(10));
+console.log(getStatus(25));
