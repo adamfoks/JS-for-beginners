30405ca43dedfa5f5e45b4356beb110a87b0a4ec
adamfoks <100434460+adamfoks@users.noreply.github.com>
2023-03-12 21:10:00 +0100

    object taksk

--- kurs-java-script.js
+++ kurs-java-script.js
@@ -13,3 +13,21 @@ myF(); //a: 1, b: 1
 myF(); //a: 2, b: 1
 myF(); //a: 3, b: 1
 myF(); //a: 4, b: 1
+
+
+
+const tab = [1,2,3];
+tab.length //widzisz kropkę? Przez nią odwołujemy się do metod lub właściwości obiektu - w tym przypadku obiektu typu Array
+
+const text = "Ala ma kota";
+console.log(  text.charAt(0) ); //text - typ prosty został na chwilę zamieniony na obiekt (po czym od razu po wykonaniu akcji wrócił do typu prostego)
+
+window.innerHeight //właściwość innerHeight obiektu window
+
+Math.floor(21.3); //metoda obiektu Math
+
+document.cookie //właściwość cookie obiektu document
+
+button.innerHTML //właściwość innerHTML obiektu button
+
+
